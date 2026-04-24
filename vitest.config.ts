51467b769a9c53a4bbcb9ef1b08dd4d1fdefd180
Andy Grüning <andygruening@gmail.com>
2026-04-24 16:07:35 +0200

    added tests/ directory in vite config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,7 @@ import "dotenv/config";
 
 export default defineConfig({
     test: {
-        // optional: a setup file if you need more than env loading
-        // setupFiles: ["./src/__tests__/setup.ts"],
-        fileParallelism: false, // keeps the auth flow serial
+        fileParallelism: false,
+        include: ["tests/**/*.ts"]
     },
 });
